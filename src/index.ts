c87ccd28b5a938874987f8ac56ac7d238472bf90
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2025-05-20 13:31:58 -0500

    Incorporate new session info models

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 import { ProjectClientBuilder, ProjectClient } from './project-client';
-
+export type {
+    DeviceRegisterRequest,
+    DeviceResponse,
+    GenericResponse,
+    MultimediaDetails,
+    ProjectInfo,
+    ProjectSummary,
+    NewSessionRequest,
+    ProjectSessionResponse,
+    SessionEgress,
+    SessionParticipant,
+    SessionTrack,
+    TokenRequest,
+    TokenResponse,
+    VideoGrantsWrapper,
+} from './models';
 export { ProjectClientBuilder, ProjectClient };
